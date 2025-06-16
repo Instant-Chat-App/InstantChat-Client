d41e9983a95f328ffae5b3dac745ab0319827015
anhkha30804 <anhkha30804@gmail.com>
2025-06-16 08:17:49 +0700

    feat: add edit & delete mess

---
+++ src/features/chat/components/MessagEditDelete.tsx
@@ -0,0 +1,125 @@
+import {
+   AlertDialog,
+   AlertDialogAction,
+   AlertDialogCancel,
+   AlertDialogContent,
+   AlertDialogDescription,
+   AlertDialogFooter,
+   AlertDialogHeader,
+   AlertDialogTitle,
+   AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import {
+   Dialog,
+   DialogClose,
+   DialogContent,
+   DialogFooter,
+   DialogHeader,
+   DialogTitle,
+   DialogTrigger
+} from '@/components/ui/dialog'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { PenBox, Trash } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+
+const editMessageSchema = z.object({
+   content: z.string().trim().min(1, 'Tin nhắn Không được để trống')
+})
+
+interface Props {
+   children: React.ReactNode
+   message: {
+      messageId: number
+      content: string
+   }
+}
+
+function MessagEditDelete({ message, children }: Props) {
+   const form = useForm({
+      resolver: zodResolver(editMessageSchema),
+      defaultValues: {
+         content: message.content
+      }
+   })
+
+   const onEdit = (): void => {}
+   const handleDelete = (): void => {}
+
+   return (
+      <Popover>
+         <PopoverTrigger asChild>{children}</PopoverTrigger>
+         <PopoverContent className='max-w-[150px] rounded-none p-0 font-normal'>
+            <Dialog>
+               <DialogTrigger asChild>
+                  {/*  Edit Message  */}
+                  <button className='flex w-full items-center gap-3 px-3 py-1 text-sm hover:bg-gray-100'>
+                     <PenBox className='size-3' />
+                     <span>Chỉnh sửa</span>
+                  </button>
+               </DialogTrigger>
+               <DialogContent className='sm:max-w-[425px]'>
+                  <DialogHeader>
+                     <DialogTitle>Sửa tin nhắn</DialogTitle>
+                  </DialogHeader>
+                  <Form {...form}>
+                     <form onSubmit={form.handleSubmit(onEdit)} className='space-y-4'>
+                        <FormField
+                           control={form.control}
+                           name='content'
+                           render={({ field }) => (
+                              <FormItem>
+                                 <FormControl>
+                                    <Input {...field} />
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )}
+                        />
+                        <DialogFooter>
+                           <DialogClose asChild>
+                              <Button variant='outline' type='button'>
+                                 Huỷ
+                              </Button>
+                           </DialogClose>
+                           <Button type='submit'>Đồng ý</Button>
+                        </DialogFooter>
+                     </form>
+                  </Form>
+               </DialogContent>
+            </Dialog>
+
+            <AlertDialog>
+               <AlertDialogTrigger asChild>
+                  {/*  Delete Message  */}
+                  <button
+                     className='flex w-full items-center gap-3 px-3 py-1 text-sm text-red-500 hover:bg-gray-100'
+                     onClick={handleDelete}
+                  >
+                     <Trash className='size-3' />
+                     <span>Xoá</span>
+                  </button>
+               </AlertDialogTrigger>
+               <AlertDialogContent>
+                  <AlertDialogHeader>
+                     <AlertDialogTitle>Xoá tin nhắn</AlertDialogTitle>
+                     <AlertDialogDescription>
+                        Bạn có chắc chắn muốn xoá tin nhắn này?
+                     </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                     <AlertDialogCancel>Huỷ</AlertDialogCancel>
+                     <AlertDialogAction>Đồng ý</AlertDialogAction>
+                  </AlertDialogFooter>
+               </AlertDialogContent>
+            </AlertDialog>
+         </PopoverContent>
+      </Popover>
+   )
+}
+
+export default MessagEditDelete
