b94eb3b11ddb91610fcd206e1cdd00b08a36b34e
Lezh1n <hoangphilong1208@gmail.com>
2025-06-18 00:58:05 +0700

    rename file

--- src/features/auth/types/auth.ts
+++ src/features/auth/types/auth.ts
@@ -1,6 +1,17 @@
 export interface AuthResponse {
-   accessToken: string;
-   refreshToken: string;
+   accessToken: string
+   refreshToken: string
+}
+
+export interface UserProfile {
+   id: number
+   phone: string
+   fullName: string
+   email: string
+   avatar: string | null
+   dob: Date
+   gender: 'MALE' | 'FEMALE'
+   bio: string
 }
 
 export interface ResetPasswordData {
