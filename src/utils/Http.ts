9a5ac33f9855f2602a12d385c22164f7ab238530
Lezh1n <hoangphilong1208@gmail.com>
2025-06-17 13:36:30 +0700

    refactor

--- src/utils/Http.ts
+++ src/utils/Http.ts
@@ -1,24 +1,24 @@
-import { axiosInstance } from "@/lib/Axios"
-import { DataResponse } from "@/types/DataResponse"
+import { axiosInstance } from '@/lib/Axios'
+import { DataResponse } from '@/types/DataResponse'
 
 export const http = {
    get: async <T>(url: string): Promise<T> => {
       const res = await axiosInstance.get<DataResponse<T>>(url)
-      return res.data.data
+      return res.data as T
    },
 
    post: async <T, D = unknown>(url: string, data: D): Promise<T> => {
       const res = await axiosInstance.post<DataResponse<T>>(url, data)
-      return res.data.data
+      return res.data as T
    },
 
    put: async <T, D = unknown>(url: string, data: D): Promise<T> => {
       const res = await axiosInstance.put<DataResponse<T>>(url, data)
-      return res.data.data
+      return res.data as T
    },
 
    delete: async <T>(url: string): Promise<T> => {
       const res = await axiosInstance.delete<DataResponse<T>>(url)
-      return res.data.data
+      return res.data as T
    }
 }
