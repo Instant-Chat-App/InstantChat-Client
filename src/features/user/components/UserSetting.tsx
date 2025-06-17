fd0288b447b0e95ace823ae31592d609b428de91
Lezh1n <hoangphilong1208@gmail.com>
2025-06-18 00:01:06 +0700

    add change password feat

--- src/features/user/components/UserSetting.tsx
+++ src/features/user/components/UserSetting.tsx
@@ -1,9 +1,10 @@
 import ConfirmForm from '@/components/custom/ConfirmForm'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import useAuth from '@/features/auth/hooks/useAuth'
-import { BookUser, LogOut, UserRound } from 'lucide-react'
+import { BookUser, LogOut, UserRound, Wrench } from 'lucide-react'
 import ContactList from './ContactList'
 import UpdateProfileForm from './UpdateProfileForm'
+import ChangePassword from './ChangePassword'
 
 interface Props {
    children: React.ReactNode
@@ -31,6 +32,13 @@ function UserSetting({ children }: Props) {
                   <div>Contacts</div>
                </button>
             </ContactList>
+            {/*  Change password  */}
+            <ChangePassword>
+               <button className='hover:bg-accent flex w-full items-center gap-2 rounded-md p-1'>
+                  <Wrench className='size-4' />
+                  <div>Change password</div>
+               </button>
+            </ChangePassword>
 
             {/*  Logout  */}
             <ConfirmForm
